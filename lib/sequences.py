7d9114eab86336ec079e99e9678a13026e11d17e
mwabali <martinmwash18952058@gmail.com>
2026-01-20 11:14:43 +0300

    "Added the fibonacci function"

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,15 @@
 
 def print_fibonacci(length):
     pass
+    if length <= 0:
+        print("[]")
+        return
+
+
+    fib=[0,1]
+    while len(fib) < length:
+        next_num = fib[-1] + fib[-2]
+        fib.append(next_num)
+        
+
+    print(fib[:length])
